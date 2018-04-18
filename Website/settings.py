c09094bb91cfbe846b1dbbb1721e1f325e788224
Karandeep <expert.developer.it@gmail.com>
2018-04-18 16:16:10 +0530

    update

--- Website/settings.py
+++ Website/settings.py
@@ -97,8 +97,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': 'bdadmin',
-        'PASSWORD':'testadmin@123'
     }
 }
 
