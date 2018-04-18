57b584675138c63202dda7806aac1971f05d408c
Karandeep <expert.developer.it@gmail.com>
2018-04-18 14:53:33 +0530

    settings update

--- Website/settings.py
+++ Website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!%$oi1&)!l)7)n9l&tuapp3v%-k^gk096#no4e3*pwp%d-&kv-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.billharloff.com','127.0.0.1']
 
 
 # Application definition
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
