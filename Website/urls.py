d45ac75039592b7d9f40c27c7d39a7224c4b3083
Karandeep <expert.developer.it@gmail.com>
2018-04-19 19:00:01 +0530

    latest updates

--- Website/urls.py
+++ Website/urls.py
@@ -21,9 +21,10 @@ from . import views as core_views
 from forms import LoginForm
 
 urlpatterns = [
-    url(r'^login/$', LoginUser.as_view(), name='login'),
+    url(r'', Homepage.as_view(), name='index'),
+    url(r'^login$', LoginUser.as_view(), name='login'),
     url(r'^signup$', SignupUser.as_view(), name='signup'),
-    url(r'^logout/$', Logout.as_view(), name='logout'),
+    url(r'^logout$', Logout.as_view(), name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'fiftypercentoff', OffPercent.as_view(), name='percentoff'),
     url(r'webinar$', Webinar.as_view(), name='webinar'),
@@ -51,7 +52,6 @@ urlpatterns = [
     url(r'^brozepackage$', BronzePackage.as_view(), name='brozepackage'),
     url(r'^silverpackage$', SilverPackage.as_view(), name='silverpackage'),
     url(r'^goldpackage$', GoldPackage.as_view(), name='goldpackage'),
-    url(r'', Homepage.as_view(), name='index'),
 
 
 ]
