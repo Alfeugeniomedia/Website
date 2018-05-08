cf933cfcd68c1df621e1675c8fa2d98c01a61ba8
expert developer <expert.developer.it@gmail.com>
2018-05-08 10:16:57 +0530

    credential changed fpassword

--- Website/settings.py
+++ Website/settings.py
@@ -136,7 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'mailtechnical9@gmail.com'
-EMAIL_HOST_PASSWORD = 'technical@123'
+EMAIL_HOST_PASSWORD = 'Sound@123'
 EMAIL_USE_TLS = True
 
 
